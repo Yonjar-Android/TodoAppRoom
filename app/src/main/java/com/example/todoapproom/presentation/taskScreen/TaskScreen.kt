796364fa734fd8250f33033f2daaa7b42b7dee61
Yonjar <juan132y@gmail.com>
2024-09-20 13:59:58 -0600

    Fix: Se ha implementado imePadding en el Column de TaskScreen, esto soluciona el problema que al abrir el teclado, el texto Todo App se desplazara hacia arriba

--- app/src/main/java/com/example/todoapproom/presentation/taskScreen/TaskScreen.kt
+++ app/src/main/java/com/example/todoapproom/presentation/taskScreen/TaskScreen.kt
@@ -1,7 +1,6 @@
 package com.example.todoapproom.presentation.taskScreen
 
 import android.content.Context
-import android.media.MediaPlayer
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
@@ -15,6 +14,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -42,7 +42,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -62,7 +61,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import com.example.todoapproom.R
 import com.example.todoapproom.domain.model.TaskModel
 import com.example.todoapproom.ui.theme.bgColor
 import com.example.todoapproom.ui.theme.buttonColor
@@ -89,7 +87,8 @@ fun TaskScreen(viewModel: TaskViewModel) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp),
+            .padding(10.dp)
+            .imePadding(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
